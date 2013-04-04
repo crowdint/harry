d1e2fd8d982981ed2ec78046bc9f117dfcaeebcf
David Padilla <david@crowdint.com>
2013-04-04 00:17:22 -0600

    A more descriptive application.rb file that autoloads all controller files

--- sample-app/config/application.rb
+++ sample-app/config/application.rb
@@ -1,7 +1,19 @@
-require "hogger"
-require "app/controllers/hello_controller"
+require "bundler"
 
+Bundler.require
+
+#
+# Load all controller files
+#
+Dir[File.join(File.expand_path("../../app/controllers", __FILE__), "*.rb")].each { |f| load f }
+
+#
+# Define the application
+#
 class HoggerApp < Hogger::Application
 end
 
+#
+# Set up the routes
+#
 load "config/routes.rb"
