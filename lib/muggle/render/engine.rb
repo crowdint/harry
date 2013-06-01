195e36c8af4c73b432b28aa238b2e0153246d1fc
David Padilla <david@crowdint.com>
2013-06-01 20:18:54 +0900

    Render::Engine now can read template files from disk

--- lib/muggle/render/engine.rb
+++ lib/muggle/render/engine.rb
@@ -1,5 +1,7 @@
 module Render
   class Engine
+    attr_accessor :template_path
+
     def self.engine
       @engine
     end
@@ -7,5 +9,14 @@ module Render
     def self.engine=(engine)
       @engine = engine
     end
+
+    def initialize(template_path)
+      self.template_path = template_path
+    end
+
+    def read_template
+      File.new(self.template_path).read
+    end
+
   end
 end
--- spec/muggle/render/engine_spec.rb
+++ spec/muggle/render/engine_spec.rb
@@ -8,4 +8,18 @@ describe Render::Engine do
       Render::Engine.engine.should be engine
     end
   end
+
+  describe "#read_template" do
+    it "returns the contents of the specified template file" do
+      file          = mock
+      template_path = "TEMPLATE PATH"
+      file_content  = "FILE CONTENT"
+
+      File.should_receive(:new).with(template_path).and_return(file)
+      file.should_receive(:read).and_return file_content
+
+      engine = Render::Engine.new(template_path)
+      engine.read_template
+    end
+  end
 end
